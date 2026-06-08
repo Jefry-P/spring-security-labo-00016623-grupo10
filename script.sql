946485715b9f0b54bcf3191fa3c91695ec232071
Roberto Loza <76535183+Loza64@users.noreply.github.com>
2026-06-08 13:28:07 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -18,7 +18,7 @@ CROSS JOIN permissions p
 WHERE r.name = 'USER' AND p.method = 'GET'
 ON CONFLICT DO NOTHING;
 
-insert into role_permissions(permission_id, role_id) values (13, 2); --Permiso para rol user de actualizar contraseña
+insert into role_permissions(permission_id, role_id) values (13, 2); --ejemplo para cambiar el rol a un usuario
 
 select * from role_permissions;
 select * from permissions;
